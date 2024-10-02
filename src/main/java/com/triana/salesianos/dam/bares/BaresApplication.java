0eb938566374d001f621d3883153e74ad8b93709
Manuel Gomez <gomez.maman24@triana.salesianos.edu>
2024-10-02 09:51:55 +0200

    First Commit

---
+++ src/main/java/com/triana/salesianos/dam/bares/BaresApplication.java
@@ -0,0 +1,13 @@
+package com.triana.salesianos.dam.bares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/triana/salesianos/dam/bares/BaresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triana.salesianos.dam.bares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
